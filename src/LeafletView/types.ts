46ba3d262574e4b4d1889ae01137c972d6fb9486
Carlos Ferreira <carlossts826@gmail.com>
2026-03-03 15:09:32 -0300

    feat: Add new props and Dynamically update props

--- src/LeafletView/types.ts
+++ src/LeafletView/types.ts
@@ -89,6 +89,7 @@ export interface MapEventMessage {
 
 export interface MapLayer {
   attribution?: string;
+  attributionPrefix?: string;
   baseLayer?: boolean;
   baseLayerIsChecked?: boolean;
   baseLayerName?: string;
@@ -102,6 +103,9 @@ export interface MapLayer {
   zIndex?: number;
   minZoom?: number;
   maxZoom?: number;
+  zoomControl?: boolean;
+  attributionControl?: boolean;
+  useMarkerClustering?: boolean;
 }
 
 export interface MapShape {
